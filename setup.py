015c85fb6b78984c85608258767857a243102bba
Cody Burkard <cody@dazesecurity.io>
2026-08-07 10:34:55 +0200

    fixing version pymsalruntime

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         "requests==2.31.0",
         "cryptography==50.0.0",
-        "pymsalruntime==0.2.0.6; platform_system == 'Windows'",
+        "pymsalruntime==0.20.6; platform_system == 'Windows'",
         "dataclasses==0.6",
     ],
     extras_require={
